27b0b3b5514506c2152374f956652fef364852ab
dnfarhana <dnfarhana99@gmail.com>
2023-01-18 16:48:33 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import { Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
+import Home from './routes/Home';
+import Home from './routes/About';
+import Home from './routes/Service';
+import Home from './routes/Contact';
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/about" element={<About/>}></Route>
+        <Route path="/service" element={<Service/>}></Route>
+        <Route path="/contact" element={<Contact/>}></Route>
+      </Routes>
       <Navbar></Navbar>
+      
       
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,7 +20,7 @@ class Navbar extends Component{
                     {MenuItems.map((item, index)=>{
                         return(
                             <li key={index}>
-                                <a className={item.cName} href="/"><i className={item.icon}></i>{item.title}</a>
+                                <link className={item.cName} to={item.url}><i className={item.icon}></i>{item.title}</link>
                             </li>
                         );
                     })}
